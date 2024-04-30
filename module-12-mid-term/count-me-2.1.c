41eaa06c72340554d245bb0e0a04bdae140cf56f
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-04-30 11:48:34 +0600

    solve question 3, 4 without 2d array

--- module-12-mid-term/count-me-2.1.c
+++ module-12-mid-term/count-me-2.1.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int main() {
     int T;
     scanf("%d", &T);
-    int counts[1000][3];
+    int capitaArr[T * 2];
+    int smaillArr[T * 2];
+    int digitArr[T * 2];
 
     for(int i = 0; i < T; i++) {
         int capital = 0, small = 0, digit = 0;
         char S[10001];
         scanf("%s", S);
         int stringLength = strlen(S);
+
         for(int i = 0; i < stringLength; i++) {
             char letter = S[i];
             if(letter >= 'A' && letter <= 'Z') {
@@ -21,13 +23,14 @@ int main() {
             } else {
                 digit++;
             }
-        } 
-        counts[i][0] = capital;
-        counts[i][1] = small;
-        counts[i][2] = digit;
+        }
+        capitaArr[i] = capital;
+        smaillArr[i] = small;
+        digitArr[i] = digit;
     }
+
     for(int i = 0; i < T; i++) {
-        printf("%d %d %d\n",  counts[i][0],  counts[i][1],  counts[i][2]);
+        printf("%d %d %d\n", capitaArr[i], smaillArr[i], digitArr[i]);
     }
     return 0;
 }
--- module-12-mid-term/farmars.c
+++ module-12-mid-term/farmars.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     int M1, M2, D, T;
     scanf("%d\n", &T);
-    int arr[T][3];
-
+    int result[T];
+ 
     for(int i = 0; i < T; i++) {
-        scanf("%d %d %d", &M1, &M2, &D);
-        arr[i][0] = M1;
-        arr[i][1] = M2;
-        arr[i][2] = D;
+        int arr[3];
+        for(int j = 0; j < 3; j++) {
+            scanf("%d", &arr[j]);
+        }
+
+        int M3 = arr[0] + arr[1];
+        int new_day = (arr[2] * arr[0]) / M3;
+        int reduce_day =  arr[2] - new_day;
+        result[i] = reduce_day;
+        
     }
 
-
     for(int i = 0; i < T; i++) {
-        int M3 = arr[i][0] + arr[i][1];
-        int new_day = (arr[i][2] * arr[i][0]) / M3;
-        int reduce_day =  arr[i][2] - new_day;
-        printf("%d\n", reduce_day);
+        printf("%d\n", result[i]);
     }
-
     return 0;
 }
