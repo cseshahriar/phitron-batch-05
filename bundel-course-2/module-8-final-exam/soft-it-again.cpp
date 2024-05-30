91d0aef3784afe257313ba6b44d9cc0139a82117
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-05-30 23:11:49 +0600

    cpp final exam finish

---
+++ bundel-course-2/module-8-final-exam/soft-it-again.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Student{
+    public:
+        string nm;
+        int cls;
+        char s;
+        int id;
+        float math_marks;
+        float eng_marks;
+};
+
+
+bool cmp(Student a, Student b) {
+    if((a.eng_marks) == (b.eng_marks)) {
+        // return a.id < b.id;
+        if(a.math_marks  == b.math_marks) {
+            return a.id < b.id;
+        } else {
+            return a.math_marks  > b.math_marks;
+        }
+    } else {
+        return (a.eng_marks) > (b.eng_marks);
+    }
+
+}
+
+int main() {
+    int n;
+    cin >> n;
+    Student arr[n];
+    for(int i = 0; i < n; i++) {
+        cin.ignore();
+        cin >> arr[i].nm >> arr[i].cls >> arr[i].s >> arr[i].id >> arr[i].math_marks >> arr[i].eng_marks; 
+    }
+    sort(arr, arr + n, cmp);
+    for(int i = 0; i < n; i++) {
+        cout << arr[i].nm << " " << arr[i].cls << " " << arr[i].s << " " << arr[i].id << " " << arr[i].math_marks << " " << arr[i].eng_marks << " " << endl; 
+    }
+    return 0;
+}
