cc00c7e2a96223781d6456d9a50665dd15f5058a
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-06-09 21:27:45 +0600

    complexity

---
+++ bundle-course-3-basic-ds/module-01-complexity/space-complexity.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+// input space + auxiliary space
+// max size 10^6 is possible
+int main() {
+    int n;
+    cin >> n;
+    int arr[n]; // input space
+    int brr[5]; // auxiliary space
+
+    int a, b;
+    cin >> a >> b;
+    int s = a + b; // a and b is input space // o (1)
+    // s is auxiliary space O(1)
+    return 0;
+
+}
