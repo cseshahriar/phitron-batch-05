0b947b7dc518d64ff24ccbc57beb1e6006ba6176
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-06-11 23:21:55 +0600

    range sum quer with prefix sum

---
+++ bundle-course-3-basic-ds/module-3-binary-search/Y_Range_sum_query.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+// problem range sum query
+// 1.5 * 10^7 is possible
+// but 1.5 * 10^10 occur
+// so solve with prefix sum
+
+// bruteforce is opposite of optimization 
+// position is i + 1
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    long long a[n];
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    while(q--) {
+        int l, r;
+        cin >> l >> r;
+        l--; // index = position - 1
+        r--; // index = position -1 
+        
+        int sum = 0;
+        
+        // time complexity did not match
+        for(int i=l; i<= r; i++) {
+            sum += a[i];
+        }
+
+        // optimization
+        cout << sum << endl;
+    }
+
+    return 0;
+}
--- bundle-course-3-basic-ds/module-3-binary-search/prefix-sum-array.cpp
+++ bundle-course-3-basic-ds/module-3-binary-search/prefix-sum-array.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-// bruteforce is opposite of optimization 
-// position is i + 1
 
 int main() {
     int n, q;
@@ -10,15 +8,23 @@ int main() {
     for(int i = 0; i < n; i++) {
         cin >> a[i];
     }
+
+    // prefix sum array
+    int pre[n];
+    pre[0] = a[0];
+    for(int i = 1; i < n; i++) {
+        pre[i] = a[i] + pre[i-1]; // current array value + prefix prev value
+    }
+
     while(q--) {
         int l, r;
         cin >> l >> r;
         l--; // index = position - 1
         r--; // index = position -1 
+        
         int sum = 0;
-        for(int i=l; i<= r; i++) {
-            sum += a[i];
-        }
+        if(l == 0) sum = pre[r] - 0;
+        else sum = pre[r] - pre[l-1];
         cout << sum << endl;
     }
 
