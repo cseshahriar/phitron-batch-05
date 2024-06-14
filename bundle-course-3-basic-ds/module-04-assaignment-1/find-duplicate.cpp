d5a61e7e9546606f9f03ad14a029406430946d18
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-06-14 16:54:19 +0600

    has duplicate and prefix sum array

---
+++ bundle-course-3-basic-ds/module-04-assaignment-1/find-duplicate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<long long int> a(n);
+
+    // read data
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    bool has_duplicate = false;
+    for(int i = 1; i < n; i++) {
+        if(a[i] == a[i-1]) {
+            has_duplicate = true;
+            break;
+        }
+    }
+    
+    if(has_duplicate == true) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
