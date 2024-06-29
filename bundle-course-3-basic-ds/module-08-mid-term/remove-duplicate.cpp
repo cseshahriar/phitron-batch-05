94c963b774951ef5545e7434bcb40cda941b3bdf
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-06-29 11:40:12 +0600

    remove duplicate

--- bundle-course-3-basic-ds/module-08-mid-term/remove-duplicate.cpp
+++ bundle-course-3-basic-ds/module-08-mid-term/remove-duplicate.cpp
@@ -24,26 +24,35 @@ void insert_at_tail(Node *&head, Node *&tail, long long int val) {
     tail = newNode;
 }
 
-void remove_duplicate(Node *head) {
-    if (head == NULL) return;
-    // Convert linked list to vector
-    vector<int> values;
+void remove_duplicate(Node* head) {
+    if (head == nullptr) {
+        return;
+    }
+
+    Node* current = head;
+    while (current != nullptr && current->next != nullptr) {
+        Node* runner = current;
+        while (runner->next != nullptr) {
+            if (runner->next->val == current->val) {
+                // Duplicate found, remove runner->next
+                Node* duplicate = runner->next;
+                runner->next = runner->next->next;
+                delete duplicate;
+            } else {
+                runner = runner->next;
+            }
+        }
+        current = current->next;
+    }
+}
+
+void print_linked_list(Node *head) {
     Node *temp = head;
-    while (temp != NULL) {
-        values.push_back(temp->val);
+    while(temp != NULL) {
+        cout << temp->val << " ";
         temp = temp->next;
     }
-
-    // Sort the vector
-    sort(values.begin(), values.end());
-
-    // Check for consecutive duplicates
-    for (size_t i = 1; i < values.size(); ++i) {
-        if (values[i] == values[i - 1]) {
-            // Duplicate found
-            // remove it
-        }
-    }
+    cout << endl;
 }
 
 int main() {
@@ -57,5 +66,6 @@ int main() {
         insert_at_tail(head, tail, val);
     }  
     remove_duplicate(head);
+    print_linked_list(head);
     return 0;
 }
